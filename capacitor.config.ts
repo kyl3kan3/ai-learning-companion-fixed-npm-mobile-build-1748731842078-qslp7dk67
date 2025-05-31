c3f0622dbef1a0110ecdb92aac553e4ed9947bc9
kyl3kan3 <kyl3kan3@gmail.com>
2025-05-31 17:50:49 -0500

    Add Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aiappbuilder.ailearningcompanionfixednpmmobilebuild1748731842078qslp7dk67',
+  appName: 'ai-learning-companion-fixed-npm',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
